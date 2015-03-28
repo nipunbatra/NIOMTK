54fb946f7b3e3a0d5309a4d8182ca729df76ad37
Rishi Baijal <rishi12084@iiitd.ac.in>
2015-03-28 13:55:06 +0530

    Added a new notebook with fresh plots

--- niom.py
+++ niom.py
@@ -7,7 +7,7 @@ from os import *
 from os.path import *
 from pandas import *
 
-def get_files(dir_path='/Users/rishi/Documents/Master_folder/IIITD/6th_semester/BTP/NIOMTK_datasets'):
+'''def get_files(dir_path='/Users/rishi/Documents/Master_folder/IIITD/6th_semester/BTP/NIOMTK_datasets'):
 	assert isdir(dir_path)
 	files=[join(dir_path, file) for file in listdir(dir_path) if isfile(join(dir_path, file)) and 'combed.h5' in file]
 	files.sort()
@@ -25,7 +25,7 @@ def get_average_for_current(current_file):
 	for key in store.keys():
 		value=store[key] # getting the key from the store
 		# at this point, value stores the unsampled dataset
-		data_daily_average=value.resample('D', how='mean')
+		data_daily_average=value.resample('15min', how='mean')
 		threshold=get_threshold(data_daily_average)
 		print ("Printing results only according to the mean: ")
 		print (data_daily_average > threshold)
@@ -38,18 +38,18 @@ def get_std_for_current(current_file):
         for key in store.keys():
                 value=store[key] # getting the key from the store
                 # at this point, value stores the unsampled dataset
-                data_daily_std=value.resample('D', how=np.std)
+                data_daily_std=value.resample('15min', how=np.std)
 		threshold=get_threshold (data_daily_average)
 		print ("Printing results only according to standard deviation: ")
 		print (data_daily_std > threshold)
 
-'''def get_range_for_current(current_file):
+def get_range_for_current(current_file):
 	store=HDFStore(current_file)
 	for key in store.keys():
 		value=store[key]
 		data_daily_range=value.resample('D')
 		for i in data_daily_range.index:
-'''			
+		
 def get_threshold(dataframe):
 	arr=dataframe.values
 	current_max=arr[0][1]
@@ -86,4 +86,4 @@ def get_average_threshold(dataframe):
 	return threshold
 
 get_files()
-get_daily_average_for_all()
+get_daily_average_for_all()'''
