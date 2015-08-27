a4443203f7600019fdd1827c4b63a941b69b343b
Nipun <nipunb@iiitd.ac.in>
2015-08-27 15:50:59 +0530

    Adde new stuff

--- Class.py
+++ Class.py
@@ -1,5 +1,6 @@
 from itertools import chain
 import io
+import os
 from io import *
 import pandas as pd
 from numpy import *
@@ -12,84 +13,88 @@ from StringIO import StringIO
 from sklearn import svm, datasets, metrics
 from sklearn import tree
 from sklearn.ensemble import RandomForestClassifier
-from common_functions import  *
+from common_functions import *
 from sklearn.cross_validation import train_test_split
 from sklearn.metrics import *
 
 num = 100
-path_to_csv = "/Users/Rishi/Documents/Master_folder/Semester_7/BTP/CER_both/CER Electricity Revised March 2012/Survey data - CSV format/Smart meters Residential pre-trial survey data.csv"
+path_to_csv = os.path.expanduser(
+    "~/CER/CER_both/CER Electricity Revised March 2012/Survey data - CSV format/Smart meters Residential pre-trial survey data.csv")
 
-question_list = ["Question 4704: Which of the following best describes how you cook in your home", "Question 410: What best describes the people you live with? READ OUT"]
+question_list = ["Question 4704: Which of the following best describes how you cook in your home",
+                 "Question 410: What best describes the people you live with? READ OUT"]
 
 classifiers_dict = {"SVM": svm.SVC(), "KNN": KNeighborsClassifier(n_neighbors=3),
-                    "DT": tree.DecisionTreeClassifier(),"RF":RandomForestClassifier()}
+                    "DT": tree.DecisionTreeClassifier(), "RF": RandomForestClassifier()}
 
-metric_dict = {"Precision":precision_score, "Recall":recall_score,
-               "MCC":matthews_corrcoef, "Accuracy":accuracy_score}
+metric_dict = {"Precision": precision_score, "Recall": recall_score,
+               "MCC": matthews_corrcoef, "Accuracy": accuracy_score}
+
+
 def get_list(fp, df2, question):
-	lis = []
-	d = fp[question]
-	#print "d ", d
-	if "410" in question: # of children
-		for i in list(df2.index):
-			if (d[i] == 2):
-				lis.append(0)
-			else:
-				lis.append(1)
-	if "4704" in question:
-		for i in list(df2.index):
-			if (d[i] == 1):
-				lis.append(1)
-			else:
-				lis.append(0)
-	return lis
+    lis = []
+    d = fp[question]
+    # print "d ", d
+    if "410" in question:  # of children
+        for i in list(df2.index):
+            if (d[i] == 2):
+                lis.append(0)
+            else:
+                lis.append(1)
+    if "4704" in question:
+        for i in list(df2.index):
+            if (d[i] == 1):
+                lis.append(1)
+            else:
+                lis.append(0)
+    return lis
 
-def classData(path_to_txt = "/Users/Rishi/Downloads/file1.txt", path_to_csv = "/Users/Rishi/Documents/Master_folder/Semester_7/BTP/CER_both/CER Electricity Revised March 2012/Survey data - CSV format/Smart meters Residential pre-trial survey data.csv", question = "Question 43111: How many people under 15 years of age live in your home?"):
-	print "File: ", path_to_txt
-	print "Question: ", question
-	results = []
-	fp = pd.read_csv(path_to_csv, low_memory = False)
-	f1 = open(path_to_txt, "r")
-	data = "ID Code Power\n" + str(f1.read())
-	TESTDATA = StringIO(data)
-	df2 = DataFrame.from_csv(TESTDATA, sep=" ", parse_dates=False)
-	d = fp[question]
-	lis = get_list(fp, df2, question)
-	print len(lis)
-	df2_train = df2.head(len(df2)/2)
-	print "df2 head: ", df2.head()
-	ground_truth_train = lis[:len(df2)/2]
-	df2_test = df2.tail(len(df2)/2)
-	ground_truth_test = lis[len(df2)/2:]
 
-	dataframe_train_array = np.asarray([i for i in df2_train.values])
-	ground_truth_train_array = np.asarray(ground_truth_train)
-	dataframe_test_array = np.asarray([i for i in df2_test.values])
-	ground_truth_test_array = np.asarray(ground_truth_test)
-	print len(dataframe_train_array)#[:num]
-	print len(ground_truth_train_array)#[:num]
-	print "abc"
-	for clf_name, clf in classifiers_dict.iteritems():
-		print "Classifier: ", clf_name
-		print 1 in dataframe_train_array[:num]
-		print 0 in dataframe_train_array[:num]
-		print 1 in ground_truth_train_array[:num]
-		print 0 in ground_truth_train_array[:num]
-		clf.fit((dataframe_train_array[:num]), (ground_truth_train_array[:num]))
-        pred = clf.predict(dataframe_test_array)
-        print "Precision: ", precision_score(ground_truth_test_array[:num], pred[:num])
-        print "Recall: ", recall_score(ground_truth_test_array[:num], pred[:num])
-        print "Accuracy: ", accuracy_score(ground_truth_test_array[:num], pred[:num])
-        print "MCC: ", matthews_corrcoef(ground_truth_test_array[:num], pred[:num])
-        #for metric_name, metric_func in metric_dict.iteritems():
-        	#print clf_name, "-", metric_name, ": ", metric_func(ground_truth_test_array[:num], pred[:num])
-        	#results.append(metric_func(ground_truth_test_array, pred))
-   # print (results)
-files = ["/Users/Rishi/Downloads/file" + str(i) + ".txt" for i in range(1, 7)]
+def classData(path_to_txt="/Users/Rishi/Downloads/file1.txt",
+              path_to_csv="/Users/Rishi/Documents/Master_folder/Semester_7/BTP/CER_both/CER Electricity Revised March 2012/Survey data - CSV format/Smart meters Residential pre-trial survey data.csv",
+              question="Question 43111: How many people under 15 years of age live in your home?"):
+    print "File: ", path_to_txt
+    print "Question: ", question
+    results = []
+    fp = pd.read_csv(path_to_csv)
+    df = pd.read_csv(path_to_txt, sep=" ", names=["ID", "Code", "Power"], index_col=0)
+    d = fp[question]
+    lis = get_list(fp, df, question)
+    print len(lis)
+    df2_train = df.head(len(df) / 2)
+    print "df2 head: ", df.head()
+    ground_truth_train = lis[:len(df) / 2]
+    df2_test = df.tail(len(df) / 2)
+    ground_truth_test = lis[len(df) / 2:]
+
+    dataframe_train_array = df2_train.values
+    ground_truth_train_array = np.asarray(ground_truth_train)
+    dataframe_test_array = np.asarray([i for i in df2_test.values])
+    ground_truth_test_array = np.asarray(ground_truth_test)
+    print len(dataframe_train_array)  # [:num]
+    print len(ground_truth_train_array)  # [:num]
+    print "abc"
+    for clf_name, clf in classifiers_dict.iteritems():
+        print "Classifier: ", clf_name
+        print 1 in dataframe_train_array[:num]
+        print 0 in dataframe_train_array[:num]
+        print 1 in ground_truth_train_array[:num]
+        print 0 in ground_truth_train_array[:num]
+        clf.fit((dataframe_train_array[:num]), (ground_truth_train_array[:num]))
+    pred = clf.predict(dataframe_test_array)
+    print "Precision: ", precision_score(ground_truth_test_array[:num], pred[:num])
+    print "Recall: ", recall_score(ground_truth_test_array[:num], pred[:num])
+    print "Accuracy: ", accuracy_score(ground_truth_test_array[:num], pred[:num])
+    print "MCC: ", matthews_corrcoef(ground_truth_test_array[:num], pred[:num])
+
+
+# for metric_name, metric_func in metric_dict.iteritems():
+
+
+# print clf_name, "-", metric_name, ": ", metric_func(ground_truth_test_array[:num], pred[:num])
+# results.append(metric_func(ground_truth_test_array, pred))
+# print (results)
+files = [os.path.expanduser("~/CER/") + "file"+ str(i) + ".txt" for i in range(1, 7)]
 for f in files:
-	for question in question_list:
-		classData(f, path_to_csv, question)
-
-
-
-
+    for question in question_list:
+        classData(f, path_to_csv, question)
--- classify_occupancy.py
+++ classify_occupancy.py
@@ -34,42 +34,58 @@ def accuracy_metrics(test_gt, prediction):
     return fp
 
 def classify_train_test_split(folder_path):
-out = {}
-for season in ["summer", "winter"]:
-    out[season] = {}
-    season_path = os.path.join(folder_path, season)
-    X_train_list = []
-    y_train_list = []
-    X_test_list= []
-    y_test_list = []
+    out = {"summer":{}, "winter":{}}
+    X_test_dict = {"summer":{}, "winter":{}}
+    y_test_dict = {"summer":{}, "winter":{}}
+    X_train_dict = {"summer":[], "winter":[]}
+    y_train_dict = {"summer":[], "winter":[]}
+    y_train_list = {"summer":{}, "winter":{}}
+    X_train_list = {"summer":{}, "winter":{}}
 
 
-    for home in [1, 2, 3, 4, 5]:
-        out[season][home] = {}
-        home_path = season_path+"/"+str(home)+".csv"
-        df = pd.read_csv(home_path, index_col=0)
-        df.index = pd.to_datetime(df.index)
-        df = df.between_time("06:00", "22:00")
-        X = df
-        y = X.pop('occupancy')
-        X_train_idx,X_test_idx,y_train,y_test = train_test_split(X.index,y,test_size=0.2)
-        X_train_list.append(X.ix[X_train_idx])
-        X_test_list.append(X.ix[X_test_idx])
-        y_train_list.append(y_train)
-        y_test_list.append(y_test)
-    y_train_list=list(itertools.chain(*y_train_list))
-    y_test_list=list(itertools.chain(*y_test_list))
-    X_train_list = pd.concat(X_train_list)
-    X_test_list = pd.concat(X_test_list)
+    for season in ["summer", "winter"]:
+        print season
+        season_path = os.path.join(folder_path, season)
+        print season_path
+        for home in [1, 2, 3, 4, 5]:
+            home_path = season_path+"/"+str(home)+".csv"
+            df = pd.read_csv(home_path, index_col=0)
+            df.index = pd.to_datetime(df.index)
+            df = df.between_time("06:00", "22:00")
+            X = df
+            y = X.pop('occupancy')
+            X_train_idx,X_test_idx,y_train,y_test = train_test_split(X.index,y,test_size=0.2)
+            X_train_dict[season].append(X.ix[X_train_idx])
+            X_test_dict[season][home]=X.ix[X_test_idx]
+            y_train_dict[season].append(y_train)
+            y_test_dict[season][home]=y_test
+        y_train_list[season]=list(itertools.chain(*y_train_dict[season]))
+        X_train_list[season] = pd.concat(X_train_dict[season])
 
-        for clf_name, clf in classifiers_dict.iteritems():
-            out[season][home][clf_name] = {}
-            clf.fit(X_train, y_train)
-            pred = clf.predict(X_test)
-            for metric_name, metric_func in metric_dict.iteritems():
-                out[season][home][clf_name][metric_name] = metric_func(y_test, pred)
+    for season in ["summer", "winter"]:
+        for home in [1, 2, 3, 4, 5]:
+            out[season][home] = {}
+            for clf_name, clf in classifiers_dict.iteritems():
+                out[season][home][clf_name] = {}
+                clf.fit(X_train_list[season], y_train_list[season])
+                pred = clf.predict(X_test_dict[season][home])
+                for metric_name, metric_func in metric_dict.iteritems():
+                    out[season][home][clf_name][metric_name] = metric_func(y_test_dict[season][home], pred)
 
     return out
+
+
+def nilm_based():
+    folder_path = output_path
+    season = "summer"
+    home=1
+    season_path = os.path.join(folder_path, season)
+    home_path = season_path+"/"+str(home)+".csv"
+    df = pd.read_csv(home_path, index_col=0)
+    df.index = pd.to_datetime(df.index)
+    df = df.between_time("06:00", "22:00")
+    X = df
+    y = X.pop('occupancy')
 
 
 
@@ -100,36 +116,111 @@ def classify_train_test_same_home(folder_path):
 
 
 
-out = classify_train_test_same_home(output_path)
-res = {}
-for season, season_dict in out.iteritems():
+
+
+def plot_train_single():
+    out = classify_train_test_same_home(output_path)
+    res = {}
+    for season, season_dict in out.iteritems():
+        fig, ax = plt.subplots(nrows=3)
+        res[season] = {}
+        for metric in metric_dict.keys():
+            res[season][metric] = {}
+            for home, home_results in season_dict.iteritems():
+                res[season][metric][home] = pd.DataFrame(home_results).T.to_dict()[metric]
+        # Plotting
+    latexify(fig_height=3.2)
+    fig, ax = plt.subplots(nrows=2, sharex=True)
+    summer_df = pd.DataFrame(res['summer']['Accuracy']).T
+    winter_df = pd.DataFrame(res['winter']['Accuracy']).T
+
+    summer_df.plot(ax=ax[0], rot=0, title="Summer", kind="bar").legend(ncol=5, loc='upper center', bbox_to_anchor=(0.5, 1.2))
+    winter_df.plot(ax=ax[1], rot=0, title="Winter", kind="bar", legend=False)
+
+    format_axes(ax[0])
+    format_axes(ax[1])
+
+    ax[1].set_xlabel("Household")
+    ax[0].set_ylabel("Accuracy")
+    ax[1].set_ylabel("Accuracy")
+
+
+    #plt.tight_layout()
+    plt.savefig("figures/same_home_train.pdf", bbox_inches="tight")
+
+def plot_train_all():
+    out = classify_train_test_split(output_path)
+    res = {}
     fig, ax = plt.subplots(nrows=3)
-    res[season] = {}
-    for metric in metric_dict.keys():
-        res[season][metric] = {}
-        for home, home_results in season_dict.iteritems():
-            res[season][metric][home] = pd.DataFrame(home_results).T.to_dict()[metric]
+    for season, season_dict in out.iteritems():
+
+        res[season] = {}
+        for metric in metric_dict.keys():
+            res[season][metric] = {}
+            for home, home_results in season_dict.iteritems():
+                res[season][metric][home] = pd.DataFrame(home_results).T.to_dict()[metric]
+    # Plotting
+    latexify(fig_height=3.2)
+    fig, ax = plt.subplots(nrows=2, sharex=True)
+    summer_df = pd.DataFrame(res['summer']['Accuracy']).T
+    winter_df = pd.DataFrame(res['winter']['Accuracy']).T
+
+    summer_df.plot(ax=ax[0], rot=0, title="Summer", kind="bar").legend(ncol=5, loc='upper center', bbox_to_anchor=(0.5, 1.2))
+    winter_df.plot(ax=ax[1], rot=0, title="Winter", kind="bar", legend=False)
+
+    format_axes(ax[0])
+    format_axes(ax[1])
+
+    ax[1].set_xlabel("Household")
+    ax[0].set_ylabel("Accuracy")
+    ax[1].set_ylabel("Accuracy")
 
 
-# Plotting
-latexify(fig_height=3.2)
-fig, ax = plt.subplots(nrows=2, sharex=True)
-summer_df = pd.DataFrame(res['summer']['Accuracy']).T
-winter_df = pd.DataFrame(res['summer']['Accuracy']).T
+    #plt.tight_layout()
+    plt.savefig("figures/split.pdf", bbox_inches="tight")
 
-summer_df.plot(ax=ax[0], rot=0, title="Summer", kind="bar").legend(ncol=5, loc='upper center', bbox_to_anchor=(0.5, 1.2))
-winter_df.plot(ax=ax[1], rot=0, title="Winter", kind="bar", legend=False)
+def plot_train_all_drop():
+    out1 = classify_train_test_same_home(output_path)
+    out2 = classify_train_test_split(output_path)
+    res1 = {}
+    res2= {}
+    for season, season_dict in out1.iteritems():
 
-format_axes(ax[0])
-format_axes(ax[1])
+        res1[season] = {}
+        for metric in metric_dict.keys():
+            res1[season][metric] = {}
+            for home, home_results in season_dict.iteritems():
+                res1[season][metric][home] = pd.DataFrame(home_results).T.to_dict()[metric]
+    for season, season_dict in out2.iteritems():
 
-ax[1].set_xlabel("Household")
-ax[0].set_ylabel("Accuracy")
-ax[1].set_ylabel("Accuracy")
+        res2[season] = {}
+        for metric in metric_dict.keys():
+            res2[season][metric] = {}
+            for home, home_results in season_dict.iteritems():
+                res2[season][metric][home] = pd.DataFrame(home_results).T.to_dict()[metric]
+        # Plotting
+    latexify(fig_height=3.2)
+    fig, ax = plt.subplots(nrows=2, sharex=True)
+    summer_df_1 = pd.DataFrame(res1['summer']['Accuracy']).T
+    winter_df_1 = pd.DataFrame(res1['winter']['Accuracy']).T
+
+    summer_df_2 = pd.DataFrame(res2['summer']['Accuracy']).T
+    winter_df_2 = pd.DataFrame(res2['winter']['Accuracy']).T
+
+    (summer_df_1-summer_df_2).plot(ax=ax[0], rot=0, title="Summer", kind="bar").legend(ncol=5, loc='upper center', bbox_to_anchor=(0.5, 1.2))
+    (winter_df_1-winter_df_2).plot(ax=ax[1], rot=0, title="Winter", kind="bar", legend=False)
+
+    format_axes(ax[0])
+    format_axes(ax[1])
+
+    ax[1].set_xlabel("Household")
+    ax[0].set_ylabel("Accuracy")
+    ax[1].set_ylabel("Accuracy")
 
 
-#plt.tight_layout()
-plt.savefig("figures/same_home_train.pdf", bbox_inches="tight")
+    #plt.tight_layout()
+    plt.savefig("figures/split.pdf", bbox_inches="tight")
+
 
 
 
